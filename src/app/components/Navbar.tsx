96b7a0f6131b73032f6bbc72c512f5bd69408679
Isfhan Ahmed <isfhan729@gmail.com>
2024-11-22 02:35:19 +0500

    update navbar component with complete basic auth

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,20 +1,66 @@
 // Import stuff from next
-import Image from "next/image";
 import Link from "next/link";
 
-const Navbar: React.FC = () => {
+// Import stuff from next-auth
+import { auth, signIn, signOut } from "@/app/auth";
+
+const Navbar: React.FC = async () => {
+  // Get the session from next-auth
+  const session = await auth();
+
   return (
-    <header className="px-5 py-3  bg-blue-300 shadow-sm font-work-sans">
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
+        {/* Text based Logo */}
         <Link href="/">
-          <Image
-            src="/images/logo.png"
-            alt="logo"
-            width={250}
-            height={30}
-            className="w-10"
-          />
+          <span className="text-2xl font-bold text-rose-600 border-b-4 border-b-stone-900 font-work-sans">
+            Pitch
+          </span>
+          <span className="text-2xl font-bold text-stone-900 border-b-4 border-b-rose-600 font-work-sans">
+            Place
+          </span>
         </Link>
+
+        {/* Show when logged in */}
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              {/* Link to the create page */}
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+              </Link>
+
+              {/* Logout */}
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                </button>
+              </form>
+
+              {/* Link to the user page */}
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            // Login
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
